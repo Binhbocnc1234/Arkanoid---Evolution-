1f6fa9dbcad8dfc79ec2f6e24aae6b34448c329f
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2025-09-26 12:33:43 +0700

    Sửa lỗi cho file Ball

--- Ball.java
+++ Ball.java
@@ -1,6 +1,6 @@
 import gobj.*;
 
-public class Ball extends GameObject{
+public class Ball extends MovableObject{
     protected Ball(float x, float y, float width, float height, String imagePath) {
         super(x, y, width, height, imagePath);
     }
