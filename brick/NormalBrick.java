da0e3e9e5f9596e9621a66c7e4e948fd33b12a36
Vanillinium <trantuancuong458@gmail.com>
2025-10-21 21:38:38 +0700

    chore: Attempt to add particle
    Issue: Cannot add particles into objectList.

--- brick/NormalBrick.java
+++ brick/NormalBrick.java
@@ -1,9 +1,25 @@
 package brick;
 
+import javax.swing.SwingUtilities;
+
+import info.GameInfo;
+
 public class NormalBrick extends Brick {
     public static final int ID = 1;
 
     public NormalBrick(float x, float y, float w, float h) {
         super(x, y, w, h, 1, 0.05f, ID);
+    }
+
+    @Override
+    public void onDestroyed() {
+        int count = 3 + (int) (Math.random() * 3);
+        System.out.println("Creating " + count + " brick particles...");
+
+        for (int i = 0; i < count; i++) {
+                BrickParticle particle = new BrickParticle(x, y);
+                GameInfo.getInstance().getObjects().add(particle);
+                System.out.println("Particle created at (" + x + ", " + y + ")");
+        }
     }
 }
