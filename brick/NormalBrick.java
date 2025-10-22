6ced80566503f3cea160324856674fd728a3cd6c
TAnhgioi <tranhve90@gmail.com>
2025-10-22 11:11:57 +0700

    feat(brick): add method drop PowerUp, add new attribute Random seed to use method

--- brick/NormalBrick.java
+++ brick/NormalBrick.java
@@ -6,7 +6,7 @@ public class NormalBrick extends Brick {
     public static final int ID = 1;
 
     public NormalBrick(float x, float y, float w, float h) {
-        super(x, y, w, h, 1, 0.05f, ID);
+        super(x, y, w, h, 1, 0.3f, ID);
     }
 
     @Override
