7ee0dbbb87577a44aec35ca8c99bcaaf0d75e17f
TAnhgioi <tranhve90@gmail.com>
2025-10-31 10:53:41 +0700

    chore(NormalBrick): change dropChance

--- brick/NormalBrick.java
+++ brick/NormalBrick.java
@@ -4,6 +4,6 @@ public class NormalBrick extends Brick {
     public static final int ID = 1;
 
     public NormalBrick(float x, float y, float w, float h) {
-        super(x, y, w, h, 1, 0.05f, ID,100);
+        super(x, y, w, h, 1, 0.1f, ID,100);
     }
 }
