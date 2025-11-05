5fefb25c32a029a046e3663bea329555501293a6
TAnhgioi <tranhve90@gmail.com>
2025-11-05 08:23:26 +0700

    test: add JUnit tests

---
+++ src/test/java/game/brick/BrickTest.java
@@ -0,0 +1,23 @@
+package brick;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NormalBrickTest {
+    @Test
+    public void takeDamageTest() {
+        Brick b = BrickFactory.createBrick(2, 0, 0, 0, 0);
+        b.takeDamage(1);
+        assertEquals(1, b.hp);
+    }
+
+    @Test
+    public void obsidianTakeDamageTest () {
+        Brick b = BrickFactory.createBrick(5, 0, 0, 0, 0);
+        int baseHp = b.hp;
+        b.takeDamage(2);
+        assertEquals(baseHp, b.hp);
+    }
+    
+    
+}
